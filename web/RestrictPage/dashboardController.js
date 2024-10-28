1abce23654102909a01ae318e3de57eb6afb31ff
nicolasruoco <nicolas.ruoco@germinare.org.br>
2024-10-28 12:32:59 -0300

    feat: adding the link to the dashboard

--- web/RestrictPage/dashboardController.js
+++ web/RestrictPage/dashboardController.js
@@ -1,4 +1,4 @@
-const dashboardsUrl = ["http://endless.horse/", "https://app.powerbi.com/groups/me/reports/bb34f5b7-11c1-4808-9b34-797ca757ab27?pbi_source=desktophttps://app.powerbi.com/links/WGpWaLyvKz?ctid=b148f14c-2397-402c-ab6a-1b4711177ac0&pbi_source=linkShare", "https://gohorse.com.br/extreme-go-horse-xgh/"];
+const dashboardsUrl = ["https://app.powerbi.com/view?r=eyJrIjoiN2U1NTFmM2YtMTg3YS00OWMyLTgyNWEtN2QxNmIxZTYxNTIyIiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9", "http://endless.horse/"];
 
 const btAvancar = document.getElementById("btAvancar");
 const btVoltar = document.getElementById("btVoltar");
